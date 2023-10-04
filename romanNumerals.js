6219c2e9e01353170cd09bee3f3e7dfdf21ae060
Mattlewis2014@gmail.com <mattlewis2014@gmail.com>
2023-10-04 23:12:40 +0200

    finished toRoman fnct

--- romanNumerals.js
+++ romanNumerals.js
@@ -29,11 +29,57 @@ toRomanLazy(4); // 'IIII'
 toRomanLazy(150); // 'CL'
 toRomanLazy(944); // 'DCCCCXXXXIIII'
 
-// function toRoman(num) {
-//   return "";
-// }
+function toRoman(num) {
+  let output = "";
+  var romanNumeralToArabic = {
+    I: 1,
+    IV: 4,
+    V: 5,
+    IX: 9,
+    X: 10,
+    XL: 40,
+    L: 50,
+    C: 100,
+    CD: 400,
+    D: 500,
+    CM: 900,
+    M: 1000,
+  };
+  var romanNumeralPriorityOrder = [
+    "M",
+    "CM",
+    "D",
+    "CD",
+    "C",
+    "L",
+    "XL",
+    "X",
+    "IX",
+    "V",
+    "IV",
+    "I",
+  ];
+  for (ltr of romanNumeralPriorityOrder) {
+    if (num / romanNumeralToArabic[ltr] < 1) {
+    } else if (num / romanNumeralToArabic[ltr] >= 1) {
+      howMany = Math.floor(num / romanNumeralToArabic[ltr]);
+      //how many times the roman numeral will go into sum
+      for (i = howMany; i > 0; i--) {
+        output += ltr;
+        num -= romanNumeralToArabic[ltr];
+      }
+    }
+  }
+  console.log(output);
+  return output;
+}
 
-// toRoman(4); // 'IV'
-// toRoman(150); // 'CL'
-// toRoman(944); // 'CMXLIV'
-// module.exports = { toRoman, toRomanLazy };
+toRoman(4); // 'IV'
+toRoman(150); // 'CL'
+toRoman(944); // 'CM XL IV'
+module.exports = { toRoman, toRomanLazy };
+// IV -> 4
+// IX -> 9
+// XL -> 40
+// CD -> 400
+// CM -> 900
